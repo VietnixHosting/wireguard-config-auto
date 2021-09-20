79ebc5a5da6b717e7ab6b0c0ba226eab1c0b43d4
Hung Nguyen <hung.nguyen@vietnix.com.vn>
2021-09-20 09:49:53 +0700

    Add README and setup script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# wireguard-config-auto
-Shell script to auto install &amp; config Wireguard on CentOS 7,8 and Ubuntu Server
+# WIREGUARD VPN SETUP & CONFIG AUTOMATION
+
+Auto setup & config Wireguard VPN for CentOS 7,8 and Ubuntu Server
+
+## PREPARE
+Change these parameters to match your setup
+
+- MAX_CLIENTS="5" # Number of client config will be generated
+- SERVER_IP="192.168.0.10" # Your server public IP Adress that client will connect to
+- SERVER_PORT="56789" # Define port that VPN Service will listen on (UDP)
+- DEVICE="ens192" # Name of the network interface corresponding to SERVER_IP (used in iptables NAT rule)
+- DISABLE_SELINUX="0" # CHANGE to 1: If you want to disable selinux (CentOS)
+- DISALBE_FIREWALLD="0" # CHANGE to 1: If you want to disable Firewalld (CentOS)
+- TUNNEL_ADDR_PREFIX="10.8.0" # Local IP address for client after connect to VPN Server
+- ROUTES="0.0.0.0/0" # Define ip addresses that will be routed through VPN Tunnel (0.0.0.0/0 mean ALL traffic)
+
+## USAGE
+After change default variables, run this command to begin install Wireguard VPN & Config
+
+```bash
+bash wireguard_setup.sh
+```
+
+When setup completed, VPN client's configuration file will be found at "/etc/wireguard/keys" directory. Just copy "*.conf" file (Example: client1.conf) and import to Wireguard client app to connect
+
+## READ MORE
+- https://vietnix.vn/wireguard-la-gi/
+
---
+++ wireguard_setup.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+
+# Server config
+MAX_CLIENTS="5"
+SERVER_IP="192.168.0.10" # CHANGE ME
+SERVER_PORT="56789" # CHANGE ME
+DEVICE="ens192" # CHANGE ME
+DISABLE_SELINUX="0" # CHANGE to 1: If you want to disable selinux (CentOS)
+DISALBE_FIREWALLD="0" # CHANGE to 1: If you want to disable Firewalld (CentOS)
+TUNNEL_ADDR_PREFIX="10.8.0"
+ROUTES="0.0.0.0/0" # Define ip addresses that will be routed through VPN Tunnel (0.0.0.0/0 mean ALL traffic)
+
+# Config paths
+SERVER_CONFIG="/etc/wireguard/wg0.conf"
+KEYS_DIR="/etc/wireguard/keys"
+
+# iptables full path
+IPT=$(which iptables)
+
+# init directory
+[ ! -d $KEYS_DIR ] && mkdir -p $KEYS_DIR
+
+function install_centos() {
+	# Disable SElinux
+	if [[ "$DISABLE_SELINUX" -eq 1 ]]
+	then
+		sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
+		setenforce 0
+	fi
+
+	# Disable firewalld
+	if [[ "$DISABLE_FIREWALLD" -eq 1 ]]
+	then
+		systemctl stop firewalld
+		systemctl disable firewalld
+	fi
+
+	# Get CentOS Version
+	eval $(cat /etc/os-release | grep "VERSION_ID=")
+	if [[ "$VERSION_ID" -eq 7 ]]
+	then
+		yum install epel-release elrepo-release -y
+		yum install yum-plugin-elrepo -y
+		yum install kmod-wireguard wireguard-tools -y
+	fi
+
+	if [[ "$VERSION_ID" -eq 8 ]]
+	then
+		yum install elrepo-release epel-release -y
+		yum install kmod-wireguard wireguard-tools -y
+	fi
+
+	# load module
+	modprobe wireguard
+}
+
+function install_ubuntu() {
+	apt install wireguard -y
+}
+
+function install() {
+	if [[ -f /etc/redhat-release ]]
+	then
+		grep -q CentOS /etc/redhat-release && install_centos
+	fi
+
+	if [[ -f /etc/lsb-release ]]
+	then
+		grep -q Ubuntu /etc/lsb-release && install_ubuntu
+	fi
+
+	# enable ip forwarding
+	if [[ -z "$(grep ip_forward /etc/sysctl.conf)" ]]
+	then
+		echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+		sysctl -w net.ipv4.ip_forward=1 
+	fi
+}
+
+function gen_keys() {
+	# gen server keys
+	if [ ! -f "${KEYS_DIR}/server_private.key" ]
+	then
+		echo "Generating server keys: "
+		wg genkey | tee "${KEYS_DIR}/server_private.key" | wg pubkey > "${KEYS_DIR}/server_public.key"
+	else
+		echo "The Server key already exists: $KEYS_DIR/server_private.key"
+		echo "Please remove it and try again!"
+		exit
+	fi
+
+	# gen client keys
+	for i in `seq 1 $MAX_CLIENTS`
+	do
+		client_name="client${i}"
+		if [ ! -f "$KEYS_DIR/$client_name" ]
+		then
+			echo "Generating client keys: $client_name"
+			wg genkey | tee "${KEYS_DIR}/${client_name}_private.key" | wg pubkey > "${KEYS_DIR}/${client_name}_public.key"
+		else
+			echo "Client already exists: $client_name"
+			echo "Please remove it and try again!"
+			exit
+		fi
+	done
+}
+
+function gen_server_config() {
+	# backup current config if exists
+	if [ -f "$SERVER_CONFIG" ] 
+	then
+		mv "$SERVER_CONFIG" "${SERVER_CONFIG}_$(date +%s)"
+	fi
+
+	server_pri_key=$(cat "${KEYS_DIR}/server_private.key")
+
+	# Server base config
+	cat > $SERVER_CONFIG <<EOF
+[Interface]
+PrivateKey =  $server_pri_key
+Address = ${TUNNEL_ADDR_PREFIX}.254/24
+SaveConfig = true
+ListenPort = ${SERVER_PORT}
+PostUp = $IPT -A FORWARD -i wg0 -j ACCEPT; $IPT -t nat -A POSTROUTING -s ${TUNNEL_ADDR_PREFIX}.0/24 -o ${DEVICE} -j MASQUERADE
+PostDown = $IPT -D FORWARD -i wg0 -j ACCEPT; $IPT -t nat -D POSTROUTING -s ${TUNNEL_ADDR_PREFIX}.0/24 -o ${DEVICE} -j MASQUERADE
+
+EOF
+
+	# Append client config to server
+	for i in `seq 1 $MAX_CLIENTS`
+	do
+		client_name="client${i}"
+		if [ -f "${KEYS_DIR}/${client_name}_public.key" ]
+		then
+			client_pub_key=$(cat ${KEYS_DIR}/${client_name}_public.key)
+			cat >> $SERVER_CONFIG <<EOF
+[Peer]
+PublicKey = $client_pub_key
+AllowedIPs = $TUNNEL_ADDR_PREFIX.$i
+
+EOF
+		else
+			echo "Client key not found: $client_name"
+		fi
+	done
+
+	chmod 600 "$SERVER_CONFIG"
+
+}
+
+function gen_client_config() {
+	for i in `seq 1 $MAX_CLIENTS`
+	do
+		client_name="client${i}"
+		if [ ! -f "$KEYS_DIR/${client_name}_private.key" ]
+		then
+			echo "[WARN] Client key not found: $client_name"
+			continue
+		fi
+
+		client_pri_key=$(cat $KEYS_DIR/${client_name}_private.key)
+		server_pub_key=$(cat ${KEYS_DIR}/server_public.key)
+		echo "Generating config for $client_name"
+
+		# backup current config if exists
+		if [ -f "$KEYS_DIR/${client_name}.conf" ]
+		then
+			mv "$KEYS_DIR/${client_name}.conf" "$KEYS_DIR/${client_name}.conf_$(date +%s)"
+		fi
+
+		cat > "$KEYS_DIR/${client_name}.conf" <<EOF
+[Interface]
+PrivateKey = $client_pri_key
+Address = $TUNNEL_ADDR_PREFIX.${i}/24
+DNS = 1.1.1.1, 8.8.8.8
+
+[Peer]
+PublicKey = ${server_pub_key}
+Endpoint = ${SERVER_IP}:${SERVER_PORT}
+AllowedIPs = $ROUTES
+PersistentKeepalive = 21
+EOF
+	done
+}
+
+function main() {
+	if [ ! -f /usr/bin/wg ]
+	then
+		echo "Wireguard not found. Start Installing"
+		install
+	fi
+
+	echo "Wireguard found! Generating config"
+	gen_keys
+	gen_server_config
+	gen_client_config
+
+	# start service
+	wg-quick up wg0
+}
+
+main
